86c7712f75651b7a955f2e70272503b73ee458cb
yryugo <yryugo@sejuku.net>
2018-01-18 10:32:38 +0000

    ユーザー追加機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
 
-  devise_for  :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
   root  'static_pages#home'
   
