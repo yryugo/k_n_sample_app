fa78dd8c6016b49df58f46e7a83bd09dbd14312f
yryugo <yryugo@sejuku.net>
2018-01-18 11:05:35 +0000

    ユーザ編集機能実装

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
-    def after_sign_up_path_for(resource)
-        user_path(resource)
-    end
-  private
 
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+ 
+  private
+ 
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+ 
+      def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
       end
 end
