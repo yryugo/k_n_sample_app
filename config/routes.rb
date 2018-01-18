482d7597f058e03bca739201272de5aded9cd483
yryugo <yryugo@sejuku.net>
2018-01-18 11:22:06 +0000

    ユーザー一覧を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   root  'static_pages#home'
   
   
